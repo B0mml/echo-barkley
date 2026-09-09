87dd257e23d4c8c3d3432b0dbba56074cadd51f6
Lucas Deichsel <luc_dei@uni-bremen.de>
2026-09-09 02:15:58 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use esp_idf_svc::hal::i2s::config::*;
+use esp_idf_svc::hal::i2s::I2sDriver;
+use esp_idf_svc::hal::peripherals::Peripherals;
+fn main() {
+    // It is necessary to call this function once. Otherwise, some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    let config = Config::default();
+    let clock_conf = PdmRxClkConfig::from_sample_rate_hz(16_000);
+    let slot_conf =
+        PdmRxSlotConfig::from_bits_per_sample_and_slot_mode(DataBitWidth::Bits16, SlotMode::Mono)
+            .slot_mode_mask(SlotMode::Mono, PdmSlotMask::Right);
+    let gpio_conf = PdmRxGpioConfig::default();
+    let rx_conf = PdmRxConfig::new(config, clock_conf, slot_conf, gpio_conf);
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut driver = I2sDriver::new_pdm_rx(
+        peripherals.i2s0,
+        &rx_conf,
+        peripherals.pins.gpio33,
+        peripherals.pins.gpio23,
+    )
+    .unwrap();
+
+    println!("Connected to mic!...");
+
+    let mut buffer = [0u8; 1024];
+
+    driver.rx_enable().unwrap();
+    loop {
+        driver
+            .read(&mut buffer, esp_idf_svc::hal::delay::BLOCK)
+            .unwrap();
+
+        let samples: Vec<_> = buffer
+            .chunks_exact(2)
+            .map(|chunk| i16::from_le_bytes([chunk[0], chunk[1]]))
+            .collect();
+
+        println!("Sample 0: {}", samples[0]);
+    }
+}
